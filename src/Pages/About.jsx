384e3e67a6b6cc2a8543efe1fafe94952357b907
dhruvang-uplers <72018139+dhruvang-uplers@users.noreply.github.com>
2021-04-07 18:59:39 +0530

    Hello

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,7 +1,7 @@
 export default function About() {
     return (
         <div>
-            About Me
+            About Me It's Meee
       <span>this is try</span>
             <h2>this is test from vraj</h2>
         </div>
