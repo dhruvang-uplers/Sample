b19ed3ebd8eea4ebf10fa99d4d778b338eeca618
dhruvang-uplers <72018139+dhruvang-uplers@users.noreply.github.com>
2021-04-09 14:19:41 +0530

    added authreducer

--- src/Redux/Auth/AuthReducer.js
+++ src/Redux/Auth/AuthReducer.js
@@ -1,30 +1,44 @@
 import * as actions from "./actionTypes";
 import { auth } from "../../firebase";
 
-export default function AuthReducer(state = [], action) {
+
+const initialState = {
+    uid: null,
+    username: null,
+    email: null,
+    refreshToken: null,
+    avatar: null,
+    state: null
+}
+
+
+export default function AuthReducer(state = initialState, action) {
     if (action.type === actions.EMAIL_SIGNUP) {
-        console.log("EMAIL_SIGNUP");
         const { email, password } = action.payload
         const data = auth.createUserWithEmailAndPassword(email, password);
         console.log(data);
 
     } else if (action.type === actions.EMAIL_SIGNIN) {
-        console.log("EMAIL_SIGNIN");
         const { email, password } = action.payload
+
         auth.signInWithEmailAndPassword(email, password).then((userCredential) => {
             var user = userCredential.user;
-            console.log(user);
-
-            return [
-                ...state,
-                {
-                    id: ++lastId,
-                    description: action.payload.description,
-                    resolved: false
-                }
-            ]
-
-
+            console.log({
+                uid: user.uid,
+                username: user.displayName,
+                email: user.email,
+                refreshToken: user.refreshToken,
+                avatar: user.photoURL,
+                state: state
+            });
+            // return state;
+            return {
+                uid: user.uid,
+                username: user.displayName,
+                email: user.email,
+                refreshToken: user.refreshToken,
+                avatar: user.photoURL
+            }
         }).catch((error) => {
             var errorCode = error.code;
             var errorMessage = error.message;
@@ -32,11 +46,10 @@ export default function AuthReducer(state = [], action) {
         });
 
     } else if (action.type === actions.EMAIL_SIGNOUT) {
-        console.log("EMAIL_SIGNOUT");
+        return state
     } else if (action.type === actions.GOOGLE_SIGNIN) {
-        console.log("GOOGLE_SIGNIN");
+        return state
     } else {
-        console.log(state);
+        return state
     }
-    return state
 }
